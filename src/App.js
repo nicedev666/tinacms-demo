6c91ee4e39a53a13212cccec90eab5bfba38ffc5
nicedev666 <80183589+nicedev666@users.noreply.github.com>
2021-03-22 21:12:23 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {TinaProvider, TinaCMS, useCMS, useForm, usePlugin} from 'tinacms'
+import logo from './Icon.svg';
+import './App.css';
+
+function App() {
+  const cms = new TinaCMS({
+    sidebar: true,
+  });
+  return (
+    <TinaProvider cms={cms}>
+      <div className="App">
+        <PageContent />
+      </div>
+    </TinaProvider>
+  );
+}
+
+export default App;
+
+const pageData = {
+  title: 'Tina is not a CMS',
+  body: 'It is a toolkit for creating a custom CMS.',
+};
+
+function PageContent() {
+  // 2. Define the form configuration object
+  const formConfig = {
+    id: 'tina-tutorial-index',
+    label: 'Edit Page',
+    fields: [
+      {
+        name: 'title',
+        label: 'Title',
+        component: 'text',
+      },
+      {
+        name: 'body',
+        label: 'Body',
+        component: 'textarea',
+      },
+    ],
+    initialValues: pageData,
+    onSubmit: async () => {
+      window.alert('Saved!')
+    },
+  }
+
+   // 3. Create the form
+   const [editableData, form] = useForm(formConfig)
+
+   // 4. Register it with the CMS
+   usePlugin(form)
+ 
+  return (
+    <section className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>{editableData.title}</h1>
+      <p>{editableData.body}</p>
+      <EditButton />
+    </section>
+  );
+}
+
+function EditButton() {
+  const cms = useCMS();
+  return (
+    <button onClick={() => cms.toggle()}>
+    +    {cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
+    </button>
+  );
+}
